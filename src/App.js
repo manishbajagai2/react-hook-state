6da0c8b22050105b054ed04054613be66ba1dda5
manishbajagai2 <manishbajagai2@gmail.com>
2022-06-18 10:57:57 +0530

    Making necessary changes and adding gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+
+  function heavyfn() {
+    console.log("I will give the initial value ");
+    return 10;
+  }
+
+  // const [count, setCount] = useState(10) // hardcoded value in the state
+  // const [count, setCount] = useState(heavyfn()) // Function runs again and again
+
+  // This function runs once -
+  // const [count, setCount] = useState(()=>heavyfn())
+  // const [count, setCount] = useState(heavyfn);
+
+  // Managing both states at once using one setState
+  // const [state, setState] = useState({count : 5, text : 'initial'})
+  // const count = state.count;
+  // const text = state.text;
+
+  // Managing states independently
+  const [count, setCount] = useState(heavyfn)
+  const [text, setText] = useState('initial')
+
+  function decreasingCount() {
+    // setCount((prevCount) => prevCount - 1);
+
+    // setState(prevCount =>{
+      // overrides another state with the new state :
+      // return { count : prevCount.count - 1 }
+
+      // mutates or copies previous state :
+      // return { ...prevCount, count : prevCount.count - 1 }
+
+      // overriding both states with a new one :
+      // return { count : prevCount.count - 1, text : 'decreasing' }
+
+    // })
+
+    setCount(prevState => prevState - 1);
+    setText('decreasing')
+
+  }
+
+  function increasingCount() {
+    // setCount((prevCount) => prevCount + 1);
+
+    // setState(prevCount =>{
+    //   return { count : prevCount.count + 1, text : prevCount.text = 'increasing' }
+    // })
+
+    setCount(prevState => prevState + 1);
+    setText('increasing')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={decreasingCount}> - </button>
+      <span> {count} </span> &ensp;
+      <span> {text} </span>
+      <button onClick={increasingCount}> + </button>
     </div>
   );
 }
